8e4a9b775cf86bca2798f9472604a4f2919e3a10
lanmower <almagestfraternite@gmail.com>
2018-07-12 21:40:07 +0200

    updtae

--- server/main.js
+++ server/main.js
@@ -1,5 +1,22 @@
 import { Meteor } from 'meteor/meteor';
-
+import { Mongo } from 'meteor/mongo';
+import steem from 'steem';
+const Posts = new Mongo.Collection('posts');
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+  "getPosts"(){
+    Posts.remove({});
+    var query = {
+      tag: 'introduceyourself',
+      limit: 10
+    };
+    steem.api.getDiscussionsByTrendingAsync(query).then(Meteor.bindEnvironment((response)=>{
+    	for(x in response) {
+    		Posts.insert(response[x]);
+    	}
+    }));
+  }
 });
